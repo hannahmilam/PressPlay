d2ca9d4a46288007cfccace63735b15caada8f40
Hannah Milam <hannah.milam16@gmail.com>
2021-10-06 16:52:10 -0600

    added ENV files

--- PressPlay.client/src/env.js
+++ PressPlay.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'harddev.us.auth0.com'
+export const clientId = 'eEA80IiNopDqPIoFZGKG2xXRdzci9Wso'
+export const audience = 'https://pressplay.com'
