02206329379ca51068501810f65600ea26c50b7a
CameronOlson <olsoncam10@gmail.com>
2021-10-07 10:14:07 -0600

    starting projects

--- PressPlay/server/controllers/ProfilesController.js
+++ PressPlay/server/controllers/ProfilesController.js
@@ -6,7 +6,16 @@ export class ProfilesController extends BaseController {
     super('api/profiles')
     this.router
       .get('', this.getProfiles)
-      .get('/:id', this.getProfile)
+      .get('/:profileId', this.getProfile)
+      .get('/:prifoileId/projects', this.getProjectsByProfileId)
+  }
+
+  async getProjectsByProfileId(req, res, next) {
+    try {
+      const profileProjects = await profileService.getProfileProjects(req.params.profileId)
+    } catch (error) {
+      next(error)
+    }
   }
 
   async getProfiles(req, res, next) {
@@ -20,7 +29,7 @@ export class ProfilesController extends BaseController {
 
   async getProfile(req, res, next) {
     try {
-      const profile = await profileService.getProfileById(req.params.id)
+      const profile = await profileService.getProfileById(req.params.profileId)
       res.send(profile)
     } catch (error) {
       next(error)
