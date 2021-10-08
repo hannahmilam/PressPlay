4883bf70babfa897c8e82afe38b9fcd914121d0b
CameronOlson <olsoncam10@gmail.com>
2021-10-08 13:45:41 -0600

    contributions

--- PressPlay.client/src/components/ContributionsCards.vue
+++ PressPlay.client/src/components/ContributionsCards.vue
@@ -1,25 +1,18 @@
 <template>
+  <h1>{{ contribution.title }}</h1>
 </template>
 
 <script>
-import { computed, onMounted } from '@vue/runtime-core'
-import Pop from '../utils/Pop'
-import { AppState } from '../AppState'
-import { contributionsService } from '../services/ContributionsService'
-import { useRoute } from 'vue-router'
+
 export default {
-  setup() {
-    const route = useRoute()
-    onMounted(async() => {
-      try {
-        await contributionsService.getcontributionsByProjectId(route.params.projectId)
-      } catch (error) {
-        Pop.toast(error.message, 'error')
-      }
-    })
-    return {
-      contributions: computed(() => AppState.contributions)
+  props: {
+    contribution: {
+      type: Object,
+      required: true
     }
+  },
+  setup() {
+    return {}
   }
 }
 </script>
--- PressPlay.client/src/services/ContributionsService.js
+++ PressPlay.client/src/services/ContributionsService.js
@@ -1,4 +1,13 @@
-class ContributionsService {
+import { AppState } from '../AppState'
+import { Contribution } from '../models/Contribution'
+import { logger } from '../utils/Logger'
+import { api } from './AxiosService'
 
+class ContributionsService {
+  async getContributionsByProjectId(projectId) {
+    const res = await api.get('api/projects/' + projectId + '/contributions')
+    logger.log('these are the contributions', res.data)
+    AppState.contributions = res.data.map(c => new Contribution(c))
+  }
 }
 export const contributionsService = new ContributionsService()
