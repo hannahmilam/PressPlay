5d7bce5f5d1a072e9ef25e701b1fee9d80e70d57
Hannah Milam <hannah.milam16@gmail.com>
2021-10-08 15:56:34 -0600

    updating profile

---
+++ PressPlay.client/src/components/Sidebar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="row" v-if="account.id">
+    <div>
+      <router-link :to="{name: 'Profile', params: {profileId: account.id}}" class="btn selectable">
+        <img :src="account.picture" height="150" class="rounded-circle" alt="">
+      </router-link>
+    </div>
+    <div>
+      <h5>
+        {{ account.name }}
+      </h5>
+    </div>
+
+    <div>
+      <p>Instruments</p>
+      <p>{{ account.instrumentTags }}</p>
+    </div>
+    <div>
+      <p>
+        Genres
+      </p>
+      <p>
+        {{ account.genreTags }}
+      </p>
+    </div>
+    <div>
+      <h5>
+        About Me:
+      </h5>
+      <p>{{ account.bio }} </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed } from '@vue/runtime-core'
+import { AppState } from '../AppState'
+export default {
+  setup() {
+    return {
+      account: computed(() => AppState.account)
+    }
+  }
+}
+</script>
+
+<style scoped lang='scss'>
+
+</style>
--- PressPlay.client/src/pages/ProfilePage.vue
+++ PressPlay.client/src/pages/ProfilePage.vue
@@ -1,17 +1,19 @@
 <template>
   <div v-if="profile">
-    <div class="container">
+    <div class="container-fluid">
       <div class="row">
-        <div class="col-12">
-          <h1>{{ profile.name }}</h1>
-          <img :src="profile.picture" alt="">
+        <div class="col-2 bg-pink">
+          <Sidebar />
         </div>
-        <div class="col-12">
-          <div class="card">
-            <div class="card-body">
-              <div class="row">
-                <!-- TODO hannah is working on this  -->
-                <ProjectsCards v-for="p in projects" :key="p.id" :project="p" />
+
+        <div class="col-10 bg-img">
+          <div class="row">
+            <div class="card bg-transparent">
+              <div class="card-body">
+                <div class="row">
+                  <!-- TODO hannah is working on this  -->
+                  <ProjectsCards v-for="p in projects" :key="p.id" :project="p" />
+                </div>
               </div>
             </div>
           </div>
@@ -53,5 +55,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.bg-pink{
+  background: white;
+  height: 100vh;
+}
+.bg-img{
+  background-image: url('https://images.unsplash.com/photo-1602292678572-16cb94ea0d88?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGJsYWNrJTIwYW5kJTIwd2hpdGUlMjBqYXp6fGVufDB8MXwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position-y: -70rem;
+}
 </style>
