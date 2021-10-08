c98610ac8eb6957d0b6c2d1eb4eb00b7d4e6d0ee
Hannah Milam <hannah.milam16@gmail.com>
2021-10-08 09:30:20 -0600

    working on home page

--- PressPlay.client/src/pages/HomePage.vue
+++ PressPlay.client/src/pages/HomePage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container-fluid px-0">
     <div class="row">
-      <img src="https://images.unsplash.com/photo-1621367507503-1b5816c754a1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" class="cover-img" alt="">
+      <img src="" class="cover-img" alt="">
     </div>
   </div>
 </template>
@@ -14,6 +14,8 @@ export default {
 
 <style scoped lang="scss">
 .cover-img{
-
+background-image: cover;
+height: 30rem;
+background-position-y: -10rem;
 }
 </style>
