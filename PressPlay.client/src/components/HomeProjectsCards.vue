4e85f58d00ed5cc9b116ff3f03e4c33540ef2104
Hannah Milam <hannah.milam16@gmail.com>
2021-10-12 16:38:59 -0600

    side scroll on homepage works

--- PressPlay.client/src/components/HomeProjectsCards.vue
+++ PressPlay.client/src/components/HomeProjectsCards.vue
@@ -1,46 +1,29 @@
 <template>
-  <router-link :to="{name: 'Project', params: {projectId: project.id}}" class="selectable text-dark">
-    <div class="card mx-3 p-3">
-      <div class="row justify-content-between">
-        <div class="col-2">
-          <router-link :to="{name: 'Profile.Originals', params: {profileId: project.creatorId}}" class="selectable">
-            <img :src="project.creator.picture" height="50" class="rounded img-f" alt="">
-          </router-link>
+  <div class="col-3">
+    <router-link :to="{name: 'Project', params: {projectId: project.id}}" class="selectable text-dark">
+      <div class="card my-3" style="width: 200px;">
+        <div class="card-header p-0" style="height: 200px;">
+          <img :src="project.creator.picture" class="rounded-top" height="198" alt="">
         </div>
-        <div class="col-6">
-          <div class="row">
-            <div class="col">
-              <p class="p-0 m-0">
+        <div class="card-body">
+          <div class="row justify-content-between me-2">
+            <div class="col-10">
+              <h5 class="card-text p-0 m-0 clip-text">
                 <b>{{ project.name }}</b>
-              </p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col">
-              <p class="p-0 m-0">
+              </h5>
+              <p class="card-text">
                 {{ project.creator.name }}
               </p>
             </div>
+            <div class="col-2">
+              <i class="mdi mdi-play f-20 selectable"></i>
+              <!-- <audio :src="project.originalMp3" controls style="width: 100px"></audio> -->
+            </div>
           </div>
         </div>
-        <div class="col-4">
-          <!-- <i class="mdi mdi-play f-20"></i> -->
-          <audio :src="project.originalMp3" controls style="width: 100px"></audio>
-        </div>
       </div>
-
-      <div class="row">
-        <div class="col-2 m-auto">
-          <button @click="toggleSpotlight()" class="spotlight-button btn-project pb-0 mb-0">
-            <p class="">
-              Spotlight
-              <i class="mdi mdi-play f-20 m-0 p-0" title="Play Spotlight" />
-            </p>
-          </button>
-        </div>
-      </div>
-    </div>
-  </router-link>
+    </router-link>
+  </div>
 </template>
 
 <script>
@@ -65,46 +48,28 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.card {
-  // border: 1px solid rgba(0, 0, 0, 0.041);
-  border-radius: 15px;
-  box-shadow: 0px 0px 6px #c027b856;
-  // box-shadow: 0px 0px 6px #c027b8c4;
-  background-color: rgba(255, 251, 251, 0.3);
-  backdrop-filter: blur(10px) saturate(100%) contrast(45%) brightness(130%);
+div.card {
+  background: white;
   display: inline-block;
-  width: 25rem;
-}
-.spotlight-button {
-position: absolute;
-transform: translateX(-20px);
-border-radius: 40%;
-
-}
-.btn-project{
-background: linear-gradient(90deg, #CB369E, #1D2EF7);
-transition: .5;
-}
-:hover.btn-project{
-background: linear-gradient(90deg, #1D2EF7, #CB369E);
-transition: .5;
-}
-.top-bar{
-   box-shadow: 0 4px 2px -2px rgba(243, 233, 233, 0.438);
-}
-
-.nav-link:hover{
-  transform: translateY(-10%);
-  font-weight: 500;
+  margin: 8px;
+  max-width: 300px;
+  perspective: 1000;
+  position: relative;
+  text-align: left;
+  transition: all 0.3s 0s ease-in;
+  width: 300px;
+  z-index: 1;
 }
 .navbar .router-link-exact-active{
   border-bottom: 2px solid var(--bs-light);
   border-bottom-left-radius: 0;
   border-bottom-right-radius: 0;
 }
-.icon-down{
-  position: absolute;
-  transform: translateY(1px);
+.card{
+  height: 280px;
 }
-
+.card:hover{
+   transform: scale(1.1);
+    transition: .5s;
+}
 </style>
--- PressPlay.client/src/pages/HomePage.vue
+++ PressPlay.client/src/pages/HomePage.vue
@@ -1,40 +1,136 @@
 <template>
-  <div class="container-fluid bg-img px-0 text-light">
-    <div class="mx-5 py-5 scrolling-wrapper">
-      <p class="my-0 ms-3">
-        Needs Guitar
-      </p>
-      <HomeProjectsCards v-for="p in needsGuitarProjects" :key="p.id" :project="p" />
+  <div class="container-fluid bg-dark text-white px-0">
+    <div class="needs-guitar">
+      <div class="row p-3">
+        <h3 class="my-0 ms-3">
+          Needs Guitar
+        </h3>
+      </div>
+
+      <div class="row ms-5">
+        <div class="col-1">
+          <button class="btn btn-prev-next" @click="scroll('left', 'guitarScroll')" v-if="true">
+            <i class="mdi mdi-chevron-left"></i>
+          </button>
+        </div>
+        <div class="col-10">
+          <div id="guitarScroll" class="row snappy-scroll">
+            <HomeProjectsCards v-for="p in needsGuitarProjects" :key="p.id" :project="p" />
+          </div>
+        </div>
+        <div class="col-1">
+          <button class="btn btn-prev-next" @click="scroll('right', 'guitarScroll')" v-if="true">
+            <i class="mdi mdi-chevron-right"></i>
+          </button>
+        </div>
+      </div>
     </div>
-    <div class="mx-5 pb-5 scrolling-wrapper">
-      <p class="my-0 ms-3">
-        Needs Vocals
-      </p>
-      <HomeProjectsCards v-for="p in needsVocalsProjects" :key="p.id" :project="p" />
+
+    <div class="needs-vocals">
+      <div class="row p-3">
+        <h3 class="my-0 ms-3">
+          Needs Vocals
+        </h3>
+      </div>
+      <div class="row ms-5">
+        <div class="col-1">
+          <button class="btn btn-prev-next" @click="scroll('left', 'vocalsScroll')" v-if="true">
+            <i class="mdi mdi-chevron-left"></i>
+          </button>
+        </div>
+        <div class="col-10">
+          <div id="vocalsScroll" class="row snappy-scroll">
+            <HomeProjectsCards v-for="p in needsVocalsProjects" :key="p.id" :project="p" />
+          </div>
+        </div>
+        <div class="col-1">
+          <button class="btn btn-prev-next" @click="scroll('right', 'vocalsScroll')" v-if="true">
+            <i class="mdi mdi-chevron-right"></i>
+          </button>
+        </div>
+      </div>
     </div>
-    <div class="mx-5 pb-5 scrolling-wrapper">
-      <p class="my-0 ms-3">
-        Needs Drums
-      </p>
-      <HomeProjectsCards v-for="p in needsDrumsProjects" :key="p.id" :project="p" />
+
+    <div class="needs-drums">
+      <div class="row p-3">
+        <h3 class="my-0 ms-3">
+          Needs Drums
+        </h3>
+      </div>
+      <div class="row ms-5">
+        <div class="col-1">
+          <button class="btn btn-prev-next" @click="scroll('left', 'drumsScroll')" v-if="true">
+            <i class="mdi mdi-chevron-left"></i>
+          </button>
+        </div>
+        <div class="col-10">
+          <div id="drumsScroll" class="row snappy-scroll">
+            <HomeProjectsCards v-for="p in needsDrumsProjects" :key="p.id" :project="p" />
+          </div>
+        </div>
+        <div class="col-1">
+          <button class="btn btn-prev-next" @click="scroll('right', 'drumsScroll')" v-if="true">
+            <i class="mdi mdi-chevron-right"></i>
+          </button>
+        </div>
+      </div>
     </div>
-    <div class="mx-5 pb-5 scrolling-wrapper">
-      <p class="my-0 ms-3">
-        Needs Bass
-      </p>
-      <HomeProjectsCards v-for="p in needsBassProjects" :key="p.id" :project="p" />
+
+    <div class="needs-bass">
+      <div class="row p-3">
+        <h3 class="my-0 ms-3">
+          Needs Bass
+        </h3>
+      </div>
+      <div class="row ms-5">
+        <div class="col-1">
+          <button class="btn btn-prev-next" @click="scroll('left', 'bassScroll')" v-if="true">
+            <i class="mdi mdi-chevron-left"></i>
+          </button>
+        </div>
+        <div class="col-10">
+          <div id="bassScroll" class="row snappy-scroll">
+            <HomeProjectsCards v-for="p in needsBassProjects" :key="p.id" :project="p" />
+          </div>
+        </div>
+        <div class="col-1">
+          <button class="btn btn-prev-next" @click="scroll('right', 'bassScroll')" v-if="true">
+            <i class="mdi mdi-chevron-right"></i>
+          </button>
+        </div>
+      </div>
     </div>
-    <div class="mx-5 scrollmenu scrolling-wrapper">
-      <p class="my-0 ms-3">
-        Needs Piano
-      </p>
-      <HomeProjectsCards v-for="p in needsPianoProjects" :key="p.id" :project="p" />
+
+    <div class="needs-piano">
+      <div class="row p-3">
+        <h3 class="my-0 ms-3">
+          Needs Piano
+        </h3>
+      </div>
+
+      <div class="row ms-5 pb-5">
+        <div class="col-1">
+          <button class="btn btn-prev-next" @click="scroll('left', 'pianoScroll')" v-if="true">
+            <i class="mdi mdi-chevron-left"></i>
+          </button>
+        </div>
+        <div class="col-10">
+          <div id="pianoScroll" class="row snappy-scroll">
+            <HomeProjectsCards v-for="p in needsPianoProjects" :key="p.id" :project="p" />
+          </div>
+        </div>
+        <div class="col-1">
+          <button class="btn btn-prev-next" @click="scroll('right', 'pianoScroll')" v-if="true">
+            <i class="mdi mdi-chevron-right"></i>
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { computed, onMounted } from '@vue/runtime-core'
+import { computed, onMounted, ref } from '@vue/runtime-core'
 import { AppState } from '../AppState'
 import Pop from '../utils/Pop'
 import { projectsService } from '../services/ProjectsService'
@@ -42,6 +138,11 @@ import { projectsService } from '../services/ProjectsService'
 export default {
   name: 'Home',
   setup() {
+    const guitarOffset = ref(0)
+    const vocalsOffset = ref(0)
+    const drumsOffset = ref(0)
+    const pianoOffset = ref(0)
+    const bassOffset = ref(0)
     onMounted(async() => {
       try {
         await projectsService.getProjects()
@@ -50,27 +151,62 @@ export default {
       }
     })
     return {
+      guitarOffset,
+      vocalsOffset,
+      drumsOffset,
+      pianoOffset,
+      bassOffset,
       needsGuitarProjects: computed(() => AppState.projects.filter(p => p.neededInstrumentTags.includes('guitar') === true)),
       needsVocalsProjects: computed(() => AppState.projects.filter(p => p.neededInstrumentTags.includes('vocals') === true)),
       needsDrumsProjects: computed(() => AppState.projects.filter(p => p.neededInstrumentTags.includes('drums') === true)),
       needsPianoProjects: computed(() => AppState.projects.filter(p => p.neededInstrumentTags.includes('piano') === true)),
-      needsBassProjects: computed(() => AppState.projects.filter(p => p.neededInstrumentTags.includes('bass') === true))
+      needsBassProjects: computed(() => AppState.projects.filter(p => p.neededInstrumentTags.includes('bass') === true)),
+      scroll(dir, elementId) {
+        const scrollAmmount = 400
+        const element = document.getElementById(elementId)
+        switch (dir) {
+          case 'right':
+            element.scrollTo({ left: element.scrollLeft + scrollAmmount, behavior: 'smooth' })
+            break
+          case 'left':
+            element.scrollTo({ left: element.scrollLeft - scrollAmmount, behavior: 'smooth' })
+            break
+        }
+      }
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
-.bg-img{
-background-image: url('https://images.unsplash.com/photo-1621367507503-1b5816c754a1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80');
-background-size: cover;
-height: 200vh;
-}
-.scrolling-wrapper {
+// .bg-img{
+// background-image: url('https://images.unsplash.com/photo-1621367507503-1b5816c754a1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80');
+// background-size: cover;
+// height: 200vh;
+// }
+.snappy-scroll{
+  flex-wrap: nowrap !important;
   overflow-x: scroll;
-  overflow-y: hidden;
-  white-space: nowrap;
-
+  scroll-snap-type: x mandatory;
+  &::-webkit-scrollbar{ }
+  &::-webkit-scrollbar-button{ /* 2 */ }
+  &::-webkit-scrollbar-track{ /* 3 */ }
+  &::-webkit-scrollbar-track-piece{  background-color: rgba(255, 255, 255, 0.541); border-radius: 45%; }
+  &::-webkit-scrollbar-thumb{ background-color: rgba(255, 255, 255, 0.541); border-radius: 45%;}
+  &::-webkit-scrollbar-corner{ /* 6 */ }
+}
+.snappy-scroll div{
+  scroll-snap-align: start;
+}
+.container-fluid{
+  height: 100%;
+  font-family: 'Source Sans Pro', sans-serif;
+}
+.btn-prev-next{
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.63);
+  color: black;
+  scroll-behavior: smooth;
 }
 
 </style>
