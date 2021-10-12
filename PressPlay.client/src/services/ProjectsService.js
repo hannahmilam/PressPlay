dc4890702f7a9a6fc969cd6860adb37a77d32eb0
Jeremy Fowler <jembyfowler@gmail.com>
2021-10-12 16:21:09 -0600

    testing source binding

--- PressPlay.client/src/services/ProjectsService.js
+++ PressPlay.client/src/services/ProjectsService.js
@@ -76,5 +76,9 @@ class ProjectsService {
     logger.log('set spotlight res', res.data)
     AppState.project = res.data
   }
+
+  async setSource(spotlightMp3, projectId) {
+    document.getElementById(projectId).innerHTML.src = spotlightMp3
+  }
 }
 export const projectsService = new ProjectsService()
