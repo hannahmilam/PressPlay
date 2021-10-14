5b94e5132c2fcf6e641a6f34ae07d22ebe6b051c
CameronOlson <olsoncam10@gmail.com>
2021-10-14 10:30:17 -0600

    comment delete if you made it

--- PressPlay.client/src/components/CommentCard.vue
+++ PressPlay.client/src/components/CommentCard.vue
@@ -1,12 +1,28 @@
 <template>
-  <div>
-    <p>
-      {{ comment.body }}
-    </p>
+  <div class="p-2 jc-around">
+    <div class="in-line">
+      <img class="small p-1" :src="comment.creator.picture" alt="">
+      <p>
+        {{ comment.body }}
+      </p>
+    </div>
+    <div>
+      <div v-if="account.id === comment.creatorId">
+        <button @click="removeComment" class="btn btn-dark">
+          Delete
+        </button>
+      </div>
+      <div v-else>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { computed } from '@vue/runtime-core'
+import { commentsService } from '../services/CommentsService'
+import Pop from '../utils/Pop'
+import { AppState } from '../AppState'
 
 export default {
   props: {
@@ -15,13 +31,36 @@ export default {
       required: true
     }
   },
-  setup() {
+  setup(props) {
     return {
+
+      account: computed(() => AppState.account),
+      async removeComment() {
+        if (await Pop.confirm()) {
+          try {
+            await commentsService.removeComment(props.comment.id)
+            Pop.toast('this has been removed')
+          } catch (error) {
+            Pop.toast(error, 'error')
+          }
+        }
+      }
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.small {
+  border-radius: 50%;
+  width: 30px
+}
+.in-line {
+display: flex;
+}
+.jc-around {
+  display: flex;
+  justify-content: space-between;
+}
 
 </style>
--- PressPlay.client/src/services/CommentsService.js
+++ PressPlay.client/src/services/CommentsService.js
@@ -6,14 +6,19 @@ class CommentsService {
   async postComment(commentData) {
     const res = await api.post('api/comments', commentData)
     logger.log('create comment data', res.data)
-    AppState.comments.push(new Comment(res.data))
-    return res.data
+    AppState.comments.push(res.data)
   }
 
   async getCommentsByProjectId(projectId) {
     const res = await api.get('api/projects/' + projectId + '/comments')
     logger.log('this is comments for the project', res.data)
     AppState.comments = res.data
+  }
+
+  async removeComment(commentId) {
+    const res = await api.delete('api/comments/' + commentId)
+    logger.log('this comment has been removed', res.data)
+    AppState.comments = AppState.comments.filter(c => c.id !== commentId)
   }
 }
 export const commentsService = new CommentsService()
