381bfcaeb66df0eed49bb9ba25d1cda253c76611
CameronOlson <olsoncam10@gmail.com>
2021-10-15 23:10:54 -0600

    play on the project page

---
+++ PressPlay.client/src/components/ProjectOriginalPlayer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="col-4 m-3">
+    <div class="card">
+      <div class="row">
+        <small>Orginal Upload</small>
+        <div class="col-10">
+          <h3>
+            {{ project.name }}
+          </h3>
+        </div>
+        <div class="col-2">
+          <!-- original Mp3 -->
+          <i :id="'pause-'+project.id" class="mdi mdi-pause f-20 selectable" @click.stop="toggleAudio" v-if="currentSong.id === project.id && playing"></i>
+          <i :id="'play-'+project.id" class="mdi mdi-play f-20 selectable" @click.stop="setSource" v-else></i>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
